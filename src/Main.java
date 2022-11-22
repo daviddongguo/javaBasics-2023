e1aae36696337d55e52adbbf3ffa9a4e79408b01
Dongguo WU <david.dong.guo@gmail.com>
2022-11-22 14:12:27 -0500

    refactor function for print full name

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,16 @@ public class Main {
 	// write your code here
         System.out.println("hello world");
 
+			printFullName();
 
-				System.out.println("please enter your lastName: ");
-				String lastName = StandardInput.readLine();
-				System.out.println("please enter your firstName: ");
-				String firstName = StandardInput.readLine();
+		}
 
-				System.out.println("Your name is : " + firstName + " " + lastName);
+	private static void printFullName() {
+		System.out.println("please enter your lastName: ");
+		String lastName = StandardInput.readLine();
+		System.out.println("please enter your firstName: ");
+		String firstName = StandardInput.readLine();
 
-    }
+		System.out.println("Your name is : " + firstName + " " + lastName);
+	}
 }
