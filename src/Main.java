5a5e9551371278ba3ff3d14d4d312b3beafebd22
Dongguo WU <38078211+dongguowu@users.noreply.github.com>
2022-11-22 18:42:26 +0000

    add unfinished code about scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+
+				Scanner scanner = new Scanner();
+
+				System.out.println("please enter your lastName");
+				String lastName = scanner.nextLine();
+				System.out.println("please enter your firstName");
+				String firstName = scanner.nextLine();
+
+				System.out.println("Your name is : " + firstName + " " + lastName);
+
     }
 }
