3f5a2af0843dd07d6254bb811914e65f5013f620
David WU <david.dong.guo@gmail.com>
2022-11-23 22:16:48 -0500

    change package name

---
+++ src/main/java/xyz/dongguo/Main.java
@@ -0,0 +1,13 @@
+package xyz.dongguo;
+
+
+import xyz.dongguo.utility.ConsoleOutput;
+
+public class Main {
+
+
+  public static void main(String[] args) {
+    ConsoleOutput.println("Java App Started.");
+
+  }
+}
