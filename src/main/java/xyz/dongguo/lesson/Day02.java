29de736e6aab8332ee2ab2ebab9a0b1ee2c78284
Dongguo WU <david.dong.guo@gmail.com>
2022-11-24 11:00:30 -0500

    format code

--- src/main/java/xyz/dongguo/lesson/Day02.java
+++ src/main/java/xyz/dongguo/lesson/Day02.java
@@ -1,18 +1,20 @@
 package xyz.dongguo.lesson;
 
 public class Day02 {
-  public static double calculateAreaOfCircle(double radius) {
+
+  public static double calculateAreaOfCircle(
+      double radius) {
     return Math.PI * radius * radius;
   }
 
-  public static String timeSecondsToString(int time){
+  public static String timeSecondsToString(int time) {
     int minutes = time / 60;
     int seconds = time % 60;
 
     return String.format("%d : %d", minutes, seconds);
   }
 
-  public static double convertCelToFah(double cel){
+  public static double convertCelToFah(double cel) {
     return cel * 9 / 5 + 32;
   }
 
