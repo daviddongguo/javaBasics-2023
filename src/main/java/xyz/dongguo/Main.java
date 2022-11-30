afb7e40559c76d3fd6e98e40c740e6cffd542b44
David WU <david.dong.guo@gmail.com>
2022-11-30 09:39:00 -0500

    addtion quiz v1

--- src/main/java/xyz/dongguo/Main.java
+++ src/main/java/xyz/dongguo/Main.java
@@ -1,11 +1,30 @@
 package xyz.dongguo;
 
-import xyz.dongguo.lesson.Day04;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    Day04.guessNumber();
+    additionQuiz();
+
+  }
+
+  private static void additionQuiz() {
+    Scanner scanner = new Scanner(System.in);
+
+    int numberA = 10 + (int)(Math.random() * 91);
+    int numberB = 10 + (int)(Math.random() * 91);
+    int result = numberA + numberB;
+
+    System.out.printf("%d + %d = ", numberA, numberB);
+    int userInput = scanner.nextInt();
+    while(result != userInput){
+      System.out.printf("%d + %d != %d, try again. %n", numberA, numberB, userInput);
+      System.out.printf("%d + %d = ", numberA, numberB);
+      userInput = scanner.nextInt();
+    }
+    System.out.printf("Yes, %d + %d = %d %n", numberA, numberB, userInput);
+    scanner.close();
   }
 }
