4415df5a6c1a86ae10a0b8eab1ff43a6a4989bd3
Dongguo WU <david.dong.guo@gmail.com>
2022-12-02 16:06:24 -0500

    log memory used

--- src/main/java/xyz/dongguo/lesson/Day07.java
+++ src/main/java/xyz/dongguo/lesson/Day07.java
@@ -16,8 +16,28 @@ public class Day07 {
 
   public void run() {
     out.println("This is Day06");
+    long usedMB = 0L;
+    out.printf("memory usage: %,dMB%n", usedMB);
+    double[] a = new double[10000000];
+    usedMB = memInfo(usedMB);
+    out.printf("memory usage: %,dMB%n", usedMB);
+    Double[] b = new Double[10000000];
+    usedMB = memInfo(usedMB);
+    out.printf("memory usage: %,dMB%n", usedMB);
+    double qq = 3.1d;
+    for (int i = 0; i < b.length; i++) {
+      b[i] = qq * Math.random();
+    }
+    usedMB = memInfo(usedMB);
+    out.printf("memory usage: %,dMB%n", usedMB);
     out.println("Press any key to continue.");
     scanner.nextLine();
+  }
+
+  private long memInfo(Long currentMemory) {
+    Runtime rt = Runtime.getRuntime();
+    long usedMB = (rt.totalMemory() - rt.freeMemory());
+    return usedMB - currentMemory;
   }
 
   public int add(int a, int b) {
