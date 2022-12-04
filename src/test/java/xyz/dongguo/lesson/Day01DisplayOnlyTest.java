47d3721129e0b2d15c68a9b911c140276d4fdb12
David WU <david.dong.guo@gmail.com>
2022-12-03 21:24:30 -0500

    use ThreadLocalRandom

---
+++ src/test/java/xyz/dongguo/lesson/Day01DisplayOnlyTest.java
@@ -0,0 +1,14 @@
+package xyz.dongguo.lesson;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class Day01DisplayOnlyTest {
+
+
+
+
+}
