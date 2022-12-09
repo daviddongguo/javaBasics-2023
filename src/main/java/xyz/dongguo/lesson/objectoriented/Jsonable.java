433ccc56e60b3b2a4032b84841e2621650df5d0f
David WU <david.dong.guo@gmail.com>
2022-12-08 23:04:25 -0500

    add Sex Enum

--- src/main/java/xyz/dongguo/lesson/objectoriented/Jsonable.java
+++ src/main/java/xyz/dongguo/lesson/objectoriented/Jsonable.java
@@ -1,11 +1,14 @@
 package xyz.dongguo.lesson.objectoriented;
 
 /**
- * @author map
+ *
+ * Has a method toJsonString() return json string
+ * @author dongguo
  */
 public interface Jsonable {
 
   /**
+   * Create a string that represent a json object.
    * @return A string represent json object
    */
   String toJsonString();
--- src/main/java/xyz/dongguo/lesson/objectoriented/Person.java
+++ src/main/java/xyz/dongguo/lesson/objectoriented/Person.java
@@ -15,27 +15,39 @@ import org.json.JSONObject;
 public class Person implements Jsonable {
 
   public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-  String name;
-  Phone phone;
+  private final String name;
   Address address;
   List<Person> kids = new ArrayList<>();
+  private SexEnum gender;
+  private Phone phone;
   private LocalDate birthDate;
 
+  public Person(String name, Phone phone, Address address, SexEnum gender) {
+    this(name, phone, address);
+    this.setGender(gender);
+  }
+
   public Person(String name, Phone phone, Address address) {
-    this.name = name;
+    this(name);
     this.phone = phone;
     this.address = address;
   }
 
-  public static void main(String[] args) {
-    Address address = new Address("123", "King Street", "Quebec City");
-    Phone phone1 = new Phone("5145131234");
-    Phone phone2 = new Phone("5145131212");
+  public Person(String name) {
+    this.name = name;
+  }
 
-    Person personAlice = new Person("Alice", phone1, address);
-    Person kidBob = new Person("Bob", null, address);
-    Person kidTom = new Person("Tom", phone2, address);
+  /**
+   * for debugging
+   *
+   * @param args no args
+   */
+  public static void main(String[] args) {
+    List<Person> roster = Person.createRoster();
+    Person personAlice = roster.get(0);
+    Person kidBob = roster.get(1);
+    Person kidTom = roster.get(2);
+
     personAlice.setAge(-5);
     personAlice.setBirthDate("1980-07-21");
     kidBob.setAge(22);
@@ -47,21 +59,42 @@ public class Person implements Jsonable {
 
     printAllJson(personAlice);
     printPrettyJson(personAlice);
-    System.out.printf("%s birthdate is %s, age is %d%n", personAlice.name, personAlice.getBirthDate(),
+    System.out.printf("%s birthdate is %s, age is %d%n", personAlice.getName(), personAlice.getBirthDate(),
        personAlice.getAge());
+    System.out.printf("%s %s order than %s%n", kidBob.getName(), kidBob.isOrderThan(kidTom) ? "is" : "is not",
+       kidTom.getName());
+    System.out.printf("%s %s order than %s%n", kidTom.getName(), kidTom.isOrderThan(kidBob) ? "is" : "is not",
+       kidBob.getName());
 
   }
 
-  private static void printAllJson(Person person1) {
+  private static void printAllJson(Person person) {
     Gson googleJson = new Gson();
-    JSONObject json = new JSONObject(googleJson.toJson(person1));
+    JSONObject json = new JSONObject(googleJson.toJson(person));
     System.out.println(json.toString(2));
-
   }
 
   private static void printPrettyJson(Jsonable obj) {
     JSONObject json = new JSONObject(obj.toJsonString());
     System.out.println(json.toString(2));
+  }
+
+  /**
+   * debugging data includes three people
+   *
+   * @return three people
+   */
+  public static List<Person> createRoster() {
+    List<Person> roster = new ArrayList<>();
+    Address address = new Address("123", "King Street", "Quebec City");
+    Phone phone1 = new Phone("5145131234");
+    Phone phone2 = new Phone("5145131212");
+
+    roster.add(new Person("Alice", phone1, address, SexEnum.FEMALE));
+    roster.add(new Person("Bob", null, address, SexEnum.MALE));
+    roster.add(new Person("Tom", phone2, address, SexEnum.MALE));
+
+    return roster;
   }
 
   public void addKid(Person kid) {
@@ -72,12 +105,62 @@ public class Person implements Jsonable {
     return this.birthDate.format(DATE_TIME_FORMATTER);
   }
 
+  public void setBirthDate(LocalDate birthDate) {
+    if (LocalDate.now().isBefore(birthDate)) {
+      return;
+    }
+    this.birthDate = birthDate;
+  }
+
   private void setBirthDate(String birthDateStr) {
     try {
-      this.birthDate = LocalDate.parse(birthDateStr);
+      setBirthDate(LocalDate.parse(birthDateStr));
     } catch (DateTimeException ex) {
       System.err.println(ex.getMessage());
     }
+  }
+
+  public boolean isOrderThan(Person person) {
+    if (this.birthDate == null || person.birthDate == null) {
+      return false;
+    }
+    return this.birthDate.isBefore(person.birthDate);
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+
+  public List<Person> getKids() {
+    return kids;
+  }
+
+  public void setKids(List<Person> kids) {
+    this.kids = kids;
+  }
+
+  public SexEnum getGender() {
+    return gender;
+  }
+
+  public void setGender(SexEnum gender) {
+    this.gender = gender;
+  }
+
+  public Phone getPhone() {
+    return phone;
+  }
+
+  public void setPhone(Phone phone) {
+    this.phone = phone;
   }
 
   @Override
@@ -99,6 +182,23 @@ public class Person implements Jsonable {
           addressStr) + String.format("%s", phoneStr)
           + String.format("%s", kidsStr);
     return String.format("{%s}", jsonBody);
+  }
+
+  public int getAge() {
+    LocalDate currentDate = LocalDate.now();
+    return Period.between(birthDate, currentDate).getYears();
+  }
+
+  public void setAge(int age) {
+    boolean isValidAge = age >= 0 && age <= 150;
+    if (isValidAge) {
+      int currenYear = LocalDate.now().getYear();
+      int birthYear = currenYear - age;
+      String fakeBirthdate = birthYear + "-01-01";
+      setBirthDate(fakeBirthdate);
+      return;
+    }
+    System.err.printf("age(%d) must be larger than 0 and small than 150%n", age);
   }
 
   private String toJsonStringFromList(List<Person> list) {
@@ -126,20 +226,4 @@ public class Person implements Jsonable {
     return jsonStr.substring(1, size - 1);
   }
 
-  public int getAge() {
-    LocalDate currentDate = LocalDate.now();
-    return Period.between(birthDate, currentDate).getYears();
-  }
-
-  public void setAge(int age) {
-    boolean isValidAge = age >= 0 && age <= 150;
-    if (isValidAge) {
-      int currenYear = LocalDate.now().getYear();
-      int birthYear = currenYear - age;
-      String fakeBirthdate = birthYear + "-01-01";
-      setBirthDate(fakeBirthdate);
-      return;
-    }
-    System.err.printf("age(%d) must be larger than 0 and small than 150%n", age);
-  }
 }
