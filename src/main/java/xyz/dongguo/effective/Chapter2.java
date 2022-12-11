20d2da8e6ba25c377c0219e10dc1c59ebd882bc0
David WU <david.dong.guo@gmail.com>
2022-12-10 21:26:52 -0500

    continue learning effective java

--- src/main/java/xyz/dongguo/effective/Chapter2.java
+++ src/main/java/xyz/dongguo/effective/Chapter2.java
@@ -1,16 +1,18 @@
 package xyz.dongguo.effective;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Random;
 
+/**
+ * @author dongg
+ */
 public class Chapter2 {
-  Boolean a = Boolean.valueOf(true);
-  Boolean b = Boolean.TRUE;
 
   public static void main(String[] args){
+    Boolean a = Boolean.TRUE;
     BigInteger b = BigInteger.probablePrime(7, new Random());
 
+    System.out.println(a);
     System.out.println(b);
   }
 
