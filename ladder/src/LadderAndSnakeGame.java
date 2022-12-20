e5a333f6f86599d5c28975c386f20c2f06128a68
David WU <david.dong.guo@gmail.com>
2022-12-20 00:17:57 -0500

    update flip dice.

--- ladder/src/LadderAndSnakeGame.java
+++ ladder/src/LadderAndSnakeGame.java
@@ -285,6 +285,7 @@ public class LadderAndSnakeGame extends BaseGame {
       }
       if (list.size() >= 2) {
         for (LadderAndSnakeGamePlayer currentPlayer : list) {
+          System.out.printf("%s is throwing a dice.%n", currentPlayer.name);
           currentPlayer.diceValue = dice.getMovingSteps();
           System.out.printf("%s get a dice value of %d %n", currentPlayer.name, currentPlayer.diceValue);
         }
@@ -352,70 +353,7 @@ public class LadderAndSnakeGame extends BaseGame {
    * @return 1, 2, ... 6
    */
   private int flipDice() {
-    int result = generateRandomBetween1And6();
-    displayDice(result);
-    return result;
-  }
-
-  /**
-   * No display effect
-   *
-   * @return 1, 2, ... 6
-   */
-  private int generateRandomBetween1And6() {
-    int minDice = 1;
-    int maxDice = 6;
-    return minDice + random.nextInt(maxDice);
-  }
-
-  /**
-   * Just display No other effect
-   *
-   * @param diceValue a number that will be printed in the end
-   */
-  private void displayDice(int diceValue) {
-    long pauseTime = Setting.GAME_SPEED;
-    List<String> template = new ArrayList<>(List.of(
-       "       *                                                           ",
-       "    *    *                                                         ",
-       "  *         *                                                      ",
-       "*              *               * *                                 ",
-       "                 *           *     *          *                    ",
-       "                   *       *         *      *   *      *           ",
-       "                     *   *             *   *      *  *   * *       ",
-       "                       *                 *         *      *  * *  "
-    ));
-
-    for (int indexOfRow = 0; indexOfRow < template.size(); indexOfRow++) {
-      String string = template.get(indexOfRow);
-      char[] charArray = string.toCharArray();
-      for (int indexOfColumn = 0; indexOfColumn < charArray.length; indexOfColumn++) {
-        if (charArray[indexOfColumn] == '*') {
-          charArray[indexOfColumn] = (char) (49 + generateRandomBetween1And6());
-        }
-      }
-      boolean isLastLine = indexOfRow == template.size() - 1;
-      if (isLastLine) {
-        System.out.printf("%s ", String.copyValueOf(charArray));
-        pauseDisplay(pauseTime * 2);
-        System.out.printf("%s%n", diceValue);
-      } else {
-        System.out.println(String.copyValueOf(charArray));
-      }
-    }
-  }
-
-  /**
-   * Pause the display
-   *
-   * @param millis a time by millions
-   */
-  private void pauseDisplay(Long millis) {
-    try {
-      Thread.sleep(millis);
-    } catch (InterruptedException ex) {
-      System.err.println(ex.getMessage());
-    }
+    return dice.getMovingSteps();
   }
 
   private List<LadderAndSnakeGamePlayer> partTwo() {
