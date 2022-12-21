9f89f27e6e1567d21221a584abed5315e1d28213
David WU <david.dong.guo@gmail.com>
2022-12-20 21:51:55 -0500

    add max value of dice setting

--- ladder/src/Setting.java
+++ ladder/src/Setting.java
@@ -1,7 +1,10 @@
 public class Setting {
-  public static final long GAME_SPEED = 0L;
-  public static final boolean AUTO_RUN = true;
-  public static final boolean DEVELOPMENT_MODE = false;
+  public static final long GAME_SPEED = 200L;
+  public static final boolean AUTO_RUN = false;
   public static final int BROAD_SIZE = 100;
+  public static final int MAX_VALUE_OF_DICE = 6;
   public static final int PLAYER_START_POSITION = 70;
+
+  private Setting() {
+  }
 }
