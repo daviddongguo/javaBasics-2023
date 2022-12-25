b43962d7d858abeaf4fb2e44d4ef967347acdba0
David WU <david.dong.guo@gmail.com>
2022-12-24 22:31:47 -0500

    move files

--- src/test/java/xyz/dongguo/lesson/objectoriented/school/PersonTest.java
+++ src/test/java/xyz/dongguo/lesson/objectoriented/school/PersonTest.java
@@ -1,4 +1,4 @@
-package xyz.dongguo.lesson.objectoriented;
+package xyz.dongguo.lesson.objectoriented.school;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
